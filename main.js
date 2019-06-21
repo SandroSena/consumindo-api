6c81fcf430e0390f3c3051ce63c3a0415dafc051
Sandro Sena <sandro340@hotmail.com>
2019-06-21 13:39:16 -0300

    Fix API

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let container = document.querySelector(".container-main");
 
-fetch("http://airbnb.douglasmaia.com/api/properties")
+fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72")
   .then(response => response.json())
   .then(data => popularHtml(data));
 
